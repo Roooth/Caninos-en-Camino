525bdcebe7af86e2444355e76765675d9d1fe3d9
Roooth <142053739+Roooth@users.noreply.github.com>
2024-06-05 18:03:19 -0600

    Actualizacion de navegacion
    Ahora se puede navegar apropiadamente desde el menu principal a el minijuego "emocional"

---
+++ Caninos en Camino/Assets/Scripts/GeneralScripts/Catalogo.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Catalogo : MonoBehaviour
+{
+    public void EMOCIONAL()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void INTERACIONAL()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void FISICA()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void COGNITIVA()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
